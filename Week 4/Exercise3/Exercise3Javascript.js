9128b3a99af8af1bfd73e7401e88914f3e2ec5e1
08andersonK <33032134+08andersonK@users.noreply.github.com>
2017-11-23 17:56:24 +0000

    Week 4
    Javascript

---
+++ Week 4/Exercise3/Exercise3Javascript.js
@@ -0,0 +1,17 @@
+var isLightOn = false;
+function ToggleLight()
+{
+	if(isLightOn == false){
+		document.getElementById("lightBulb").src = "img/bulb-on.png";
+		isLightOn = true;
+	}
+	else{
+		document.getElementById("lightBulb").src = "img/bulb-off.png";
+		isLightOn = false;
+	}
+}
+
+function AddNumbers(numA,numB)
+{
+	console.log(numA + numB);
+}
